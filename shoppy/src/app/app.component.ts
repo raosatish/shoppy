17d7a2756553e3467797eb3630105d13c82f1e02
raosatish <raosatish@yahoo.com>
2020-04-12 11:32:44 -0400

    Changed Title

--- shoppy/src/app/app.component.ts
+++ shoppy/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shoppy';
+  title = 'Shoppy-your favorite shopping list app';
 }
